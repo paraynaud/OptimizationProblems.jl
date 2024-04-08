b64d94cd730bcc95cc018e727c05229961e6b800
jupall <116599171+jupall@users.noreply.github.com>
2024-04-08 12:36:34 -0400

    modifications

--- src/ADNLPProblems/julien_pallage.jl
+++ src/ADNLPProblems/julien_pallage.jl
@@ -1,4 +1,5 @@
 export julien_pallage
+using OptimalControl, Plots, JSOSolvers, NLPModels, ADNLPModels, Percival, Percival, NLPModelsModifiers, CTDirect, NLPModelsIpopt
 
 function julien_pallage(n::Int = 10, T::Float64 = 1.0, x0::Float64 = 0.1 )
 
@@ -6,9 +7,7 @@ function julien_pallage(n::Int = 10, T::Float64 = 1.0, x0::Float64 = 0.1 )
         t ∈ [ 0, T ], time
         x ∈ R, state
         u ∈ R, control
-        #γ ∈ R, state
         0 ≤ u(t) ≤ 1
-        #γ(t) ==(1+ u(t))
         x(0) == x0
         ẋ(t) == (1+u(t))*u(t)*x(t) # 
       
